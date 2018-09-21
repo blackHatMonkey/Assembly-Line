51e813c65242fef1150614aef60b1b21da1c80c7
Brian Smith <brian.smith@riseup.net>
2018-09-20 20:43:26 -0400

    Make workshop2 more efficient
    Make use of the std::count to count the number of lines instead of
    manual loop. Also fixed a bug where last line was not read.

--- ws2/Text.cpp
+++ ws2/Text.cpp
@@ -6,9 +6,9 @@
 // I confirm that the content of this file is created by me,
 // with the exception of the parts provided to me by my professor.
 
+#include <algorithm>
 #include <fstream>
-#include <iostream>
-#include <sstream>
+#include <istream>
 #include <string>
 
 #include "Text.h"
@@ -22,27 +22,26 @@ using namespace w2;
  *
  */
 
-Text::Text(const std::string& file_name) {
-        std::ifstream text_file(file_name);
+Text::Text(const std::string &file_name) {
+  std::ifstream text_file(file_name);
 
-        if (text_file.is_open()) {
-                auto line = std::string();
+  if (text_file.is_open()) {
+    // need to add one since the last new line is not read
+    capacity = std::count(std::istreambuf_iterator<char>(text_file),
+                          std::istreambuf_iterator<char>(), '\n') +
+               1;
 
-                // count the lines first to reduce allocation time
-                while (std::getline(text_file, line)) {
-                        capacity++;
-                }
+    // need to reset the position
+    text_file.seekg(0);
 
-                collection = new std::string[capacity];
+    collection = new std::string[capacity];
 
-                // need to reset the position and clear error to read again
-                text_file.clear();
-                text_file.seekg(0, text_file.beg);
-
-                for (auto i = 0; std::getline(text_file, line); i++) {
-                        collection[i] = line;
-                }
-        }
+    auto line = std::string();
+    for (auto i = 0u; i < capacity; i++) {
+      std::getline(text_file, line);
+      collection[i] = line;
+    }
+  }
 }
 
 /*
@@ -52,17 +51,17 @@ Text::Text(const std::string& file_name) {
  * @return a reference to the current object.
  *
  */
-Text& Text::operator=(const Text& other) {
-        if (this != &other) {
-                delete[] collection;
-                this->capacity = other.capacity;
-                this->collection = new std::string[capacity];
+Text &Text::operator=(const Text &other) {
+  if (this != &other) {
+    delete[] collection;
+    this->capacity = other.capacity;
+    this->collection = new std::string[capacity];
 
-                for (auto i = 0lu; i < capacity; i++) {
-                        collection[i] = other.collection[i];
-                }
-        }
-        return *this;
+    for (auto i = 0lu; i < capacity; i++) {
+      collection[i] = other.collection[i];
+    }
+  }
+  return *this;
 }
 
 /*
@@ -71,13 +70,13 @@ Text& Text::operator=(const Text& other) {
  * @param other another Text object.
  *
  */
-Text::Text(const Text& other) {
-        this->capacity = other.capacity;
-        this->collection = new std::string[capacity];
+Text::Text(const Text &other) {
+  this->capacity = other.capacity;
+  this->collection = new std::string[capacity];
 
-        for (auto i = 0lu; i < capacity; i++) {
-                collection[i] = other.collection[i];
-        }
+  for (auto i = 0lu; i < capacity; i++) {
+    collection[i] = other.collection[i];
+  }
 }
 
 /*
@@ -87,14 +86,14 @@ Text::Text(const Text& other) {
  * @return reference to the current object.
  *
  */
-Text& Text::operator=(Text&& other) noexcept {
-        if (this != &other) {
-                delete[] collection;
-                capacity = other.capacity;
-                collection = other.collection;
-                other.collection = nullptr;
-        }
-        return *this;
+Text &Text::operator=(Text &&other) noexcept {
+  if (this != &other) {
+    delete[] collection;
+    capacity = other.capacity;
+    collection = other.collection;
+    other.collection = nullptr;
+  }
+  return *this;
 }
 
 /*
@@ -103,8 +102,8 @@ Text& Text::operator=(Text&& other) noexcept {
  * @param other an rvalue of Text object.
  *
  */
-Text::Text(Text&& other) {
-        this->capacity = other.capacity;
-        this->collection = other.collection;
-        other.collection = nullptr;
+Text::Text(Text &&other) {
+  this->capacity = other.capacity;
+  this->collection = other.collection;
+  other.collection = nullptr;
 }
--- ws2/Text.h
+++ ws2/Text.h
@@ -13,19 +13,19 @@
 
 namespace w2 {
 class Text {
-       public:
-        Text() = default;
-        Text(const std::string& file_name);
-        Text(const Text& other);
-        Text& operator=(const Text& other);
-        Text(Text&& other);
-        Text& operator=(Text&& other) noexcept;
-        ~Text() { delete[] collection; };
-        size_t size() const { return capacity > 0 ? capacity + 1 : 0; };
+ public:
+  Text() = default;
+  Text(const std::string &file_name);
+  Text(const Text &other);
+  Text &operator=(const Text &other);
+  Text(Text &&other);
+  Text &operator=(Text &&other) noexcept;
+  ~Text() { delete[] collection; };
+  size_t size() const { return capacity; };
 
-       private:
-        std::string* collection = nullptr;
-        size_t capacity{0};
+ private:
+  std::string *collection = nullptr;
+  size_t capacity{0};
 };
 
 }  // namespace w2
