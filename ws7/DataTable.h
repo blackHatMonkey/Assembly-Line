826a73efc41f5a8589b9dcdb63216dde2fce4c4a
Brian Smith <brian.smith@riseup.net>
2018-10-23 14:46:17 -0400

    Add reflect file

--- ws7/DataTable.h
+++ ws7/DataTable.h
@@ -1,3 +1,11 @@
+// Name: Brian Smith
+// Seneca Student ID: 137105177
+// Seneca email: bsmith55@myseneca.ca
+// Date of completion: 23/10/2018
+//
+// I confirm that the content of this file is created by me,
+// with the exception of the parts provided to me by my professor.
+
 #ifndef W7_DATATABLE_H
 #define W7_DATATABLE_H
 
